4a4d5d59936b6481984910595850d56c95e04713
laurveni <33289796+laurveni@users.noreply.github.com>
2017-12-03 22:22:09 +0200

    completed chapter 5

--- data/create_human.R
+++ data/create_human.R
@@ -34,9 +34,50 @@ gii <- mutate(gii, lab_ratio=lab_f/lab_m)
 
 # join the two datasets using country as identifier
 human <- inner_join(hd, gii, by = "country")
+#check structure and dimensions: 195 observations and 19 variables
 str(human)
 dim(human)
 
-# save the data set in the data folder
-write.csv(human, file="human.csv",row.names=FALSE)
 
+# access the tidyr and stringr packages
+library(tidyr)
+library(stringr)
+
+# look at the structure of the gni_c column in 'human': characters
+str(human$gni_c)
+
+# remove the commas from gni_c and mutate it into numeric
+human$gni_c <- str_replace(human$gni_c, pattern=",", replace ="") %>% as.numeric
+
+# columns to keep
+keep <- c("country", "sedu_ratio", "lab_ratio", "life_exp", "exp_edu", "gni_c", "mat_mort", "ad_birth", "repr_parl")
+
+# select the 'keep' columns
+human <- select(human, one_of(keep))
+
+# completeness indicator of the 'human' data
+comp <- complete.cases(human)
+
+# filter out all rows with NA values
+human_ <- filter(human, comp)
+
+# look at the last 10 observations of human
+tail(human_, n=10)
+
+# define the last indice we want to keep
+last <- nrow(human_) - 7
+
+# choose everything until the last 7 observations
+human_ <- human_[1:last, ]
+
+# add countries as rownames
+rownames(human_) <- human_$country
+
+# remove the country variable
+human_ <- select(human_, -country)
+
+#structure of human_: 195 obs. of  8 variables
+str(human_)
+
+# save the data set in the data folder
+write.csv(human_, file="human.csv", row.names = FALSE)
